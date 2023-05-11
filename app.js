373f5b71b8965a336509d849f7e7e4269a677952
KFkas <48663613+JaoKFkas@users.noreply.github.com>
2023-05-10 22:07:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+function MakeImage(source){
+    return new Image().src = source;
+}
+
+const coisas = ["um lixo", "fofo", "meu melhor amigo", "um gostoso", "um nerd", "redpilado", "sigma", "a Ai"];
+const imagens = [MakeImage("images/lixo.png"), MakeImage("images/fofo.jpg"), MakeImage("images/amigo.jpg"),
+                MakeImage("images/gostoso.gif"), MakeImage("images/nerd.gif"), MakeImage("images/redpill.gif"),
+                MakeImage("images/sigma.gif"), MakeImage("images/ai.jpeg")];
+
+document.getElementById("ser").innerText = coisas[0];
+    document.getElementById("ilustracao").src = imagens[0];
+
+let pastValue = 0;
+
+document.getElementById("girar").addEventListener("click", function() {
+    var value = Math.round(Math.random()*(coisas.length-1));
+
+    if(value == pastValue){
+        if(value < coisas.length-1){
+            value++;
+        } else if(value > 0){
+            value--;
+        }
+    }
+
+    pastValue = value;
+
+    document.getElementById("ser").innerText = coisas[value];
+    document.getElementById("ilustracao").src = imagens[value];
+})
